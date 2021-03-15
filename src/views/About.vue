fda972271bd4120b0605811c79be32d686a7f2bc
Chrizana_Mizzi <chrizana.mizzi.c10285@mcast.edu.mt>
2021-03-15 11:21:50 +0100

    About changed

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page (Testing123234)</h1>
+    <h1>This is an about page (Commit ChangesTesting )</h1>
     <button type="button" class="btn btn-danger">Danger</button>
   </div>
 </template>
